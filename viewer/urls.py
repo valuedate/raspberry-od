5c22c845429c65e32ebf4ae00c38898a4f9ffe7e
Ricardo Fernandes Timestamp <RicardoFernandes.Timestamp@edp.com>
2025-05-03 11:48:28 +0100

    context

--- viewer/urls.py
+++ viewer/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', DashboardView.as_view(), name='dashboard'),  # Make dashboard the homepage
+    path('', views.DashboardView.as_view(), name='dashboard'),  # Make dashboard the homepage
     path('history/', views.history_view, name='history'),
     path("live/", views.live_stream_view, name="live_stream"),
     path("video_feed/", views.video_feed, name="video_feed"),
