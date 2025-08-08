74c7938fed5c6b3ddd54facbf72f26a99a0f87bb
Ricardo Fernandes <ricardo.fernandes@valuedate.pt>
2025-08-08 15:16:27 +0100

    new fix

--- viewer/models.py
+++ viewer/models.py
@@ -4,8 +4,8 @@ class Detection(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     label = models.CharField(max_length=100) # e.g., 'person', 'car_no_plate', 'car_plate_ABC123'
     image_path = models.CharField(max_length=255)
-    camera = models.CharField(max_length=255)
-    position = models.CharField(max_length=255)
+    camera = models.CharField(max_length=255, null=True, blank=True)
+    position = models.CharField(max_length=255, null=True, blank=True)
     # Optional: Store confidence score if needed
     confidence = models.FloatField(null=True, blank=True)
 
